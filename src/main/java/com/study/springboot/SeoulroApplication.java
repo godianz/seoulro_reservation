57fcf584a215bf0e000d9aa50d685175f397c5eb
godianz <godianz778@gmail.com>
2023-05-27 01:06:35 +0900

    Initial commit

---
+++ src/main/java/com/study/springboot/SeoulroApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulroApplication.class, args);
+	}
+
+}
